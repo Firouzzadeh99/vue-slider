3f147a802cb115034190c75e4790420fd6db5080
Masoumeh Firouzzadeh <mfirouzzadeh99@gmail.com>
2025-12-10 16:24:06 +0330

    feat: Github action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
+/// <reference types="node" />
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
+const rawBase = "my-slider-project"
+const normalizedBaseURL = rawBase
+  ? rawBase.startsWith('/') ? rawBase : `/${rawBase}`
+  : '/'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
   app: {
+    baseURL: normalizedBaseURL,
     head: {
       title: 'اسلایدر محصولات',
       meta: [
@@ -11,5 +20,8 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
     },
+  },
+  nitro: {
+    preset: 'github-pages',
   },
 })
